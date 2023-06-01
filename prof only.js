4123d2fe4924101f513622116c1afcac5871aac2
choudrea <135257486+choudrea@users.noreply.github.com>
2023-06-01 09:44:41 -0700

    Add files via upload

---
+++ prof only.js
@@ -0,0 +1,9 @@
+chatLangCallback({
+  "S_INFORMATION_PAGE": {
+    "S_PROFILE_LABEL": "Profile-Label-XXX",
+    "S_NAME_LABEL": "Profile_Name-XXX",
+    "S_EMAIL_LABEL": "Email_Address_Label-XXX",
+    "S_QUESTION_LABEL": "Question-XXX",
+    "S_START_BUTTON": "Start to chat XXX"
+  }
+});
